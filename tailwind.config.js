f9b90bfafbe4494c519148b42b80699248efab86
Liyana04 <liyanaaris04@gmail.com>
2024-11-25 09:46:49 +0800

    practise tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,24 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {
+    screens:{
       sm: "340px",
       md : "540px",
       lg: "768px",
       xl: "1180px"
     },
+    extend: {},
+    fontFamily:{
+      Jost: ["Jost", "sans-serif"],
+      Poppins: ["Poppins", "serif"]
+    },
+    container:{
+      center: true,
+      padding:{
+        DEFAULT: "12px",
+        md: "32px"
+      }
+    }
   },
   plugins: [],
 }
